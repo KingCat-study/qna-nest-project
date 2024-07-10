e02d1568932fc907458543ac10dbdbb052a50062
raiden <jihunrock@gmail.com>
2024-07-10 14:47:27 +0900

    - 파일 재생성

--- src/modules/user/dtos/create-request.dto.ts
+++ src/modules/user/dtos/create-request.dto.ts
@@ -2,19 +2,19 @@ import { IsEmail, IsNotEmpty, IsString, MinLength } from "class-validator";
 import { UserRole } from "../entities/user-role.enum";
 
 export class CreateUserDto {
-  @IsString()
-  @IsNotEmpty()
-  name: string;
+	@IsString()
+	@IsNotEmpty()
+	name: string;
 
-  @IsEmail()
-  @IsNotEmpty()
-  email: string;
+	@IsEmail()
+	@IsNotEmpty()
+	email: string;
 
-  @MinLength(8)
-  @IsNotEmpty()
-  password: string;
+	@MinLength(8)
+	@IsNotEmpty()
+	password: string;
 
-  @IsString()
-  role: UserRole = UserRole.USER;
+	@IsString()
+	role: UserRole = UserRole.USER;
 
 }
