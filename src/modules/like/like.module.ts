3dc9e5c01cff9b63bac6e353b59e1fe3171deba2
raiden <jihunrock@gmail.com>
2024-07-10 14:46:54 +0900

    - 사용하는 모든 엔티티 추가

--- src/modules/like/like.module.ts
+++ src/modules/like/like.module.ts
@@ -4,9 +4,11 @@ import { AuthModule } from '../auth/auth.module';
 import { Like } from './entities/like.entity';
 import { LikeController } from './like.controller';
 import { LikeService } from './like.service';
+import { Question } from '../question/entities/question.entity';
+import { Answer } from '../answer/entities/answer.entity';
 
 @Module({
-  imports: [MikroOrmModule.forFeature([Like]), AuthModule], 
+  imports: [MikroOrmModule.forFeature([Like, Question, Answer]), AuthModule], 
   providers: [
     LikeService,
   ],
